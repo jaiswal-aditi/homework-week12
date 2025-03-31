7e9b6f33919e00c772eb2bf3491b0b390804df58
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-30 16:37:51 -1000

    Update and rename bubblesort_array.c to timecard_calculator.c

---
+++ timecard_calculator.c
@@ -0,0 +1,11 @@
+/*
+Write a C program that simulates a simple timecard calculator. The user will input the clock-in
+and clock-out times in 12-hour format with AM/PM. The program should compute the total
+duration worked during a shift, even if the shift spans past midnight. It must output the 
+total time worked as a duration in hours, minutes, and seconds, without referencing AM/PM.
+
+* The program must accept two-time inputs - start of shift and end of shift.
+* 
+
+** See PDF for sample input/output
+*/
