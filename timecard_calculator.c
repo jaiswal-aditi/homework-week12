6fb8004b811b8e31112b81e13bba940703f5c446
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-30 16:48:10 -1000

    Update timecard_calculator.c

--- timecard_calculator.c
+++ timecard_calculator.c
@@ -4,11 +4,11 @@ and clock-out times in 12-hour format with AM/PM. The program should compute the
 duration worked during a shift, even if the shift spans past midnight. It must output the 
 total time worked as a duration in hours, minutes, and seconds, without referencing AM/PM.
 
-* The program must accept two-time inputs - start of shift and end of shift
-  **Input must be in 12-hour format with AM/PM:-  format -  HH MM SS AM/PM
-* Output the total duration worked in HH:MM:SS format (24-hour style, no AM/PM).
-* Shfts can span across midnight.
-* program assumes all shifts occur within a 24-hour window (no multi-day shifts)
+- The program must accept two-time inputs - start of shift and end of shift.
+ - Input must be in 12-hour format with AM/PM:-  format -  HH MM SS AM/PM
+- Output the total duration worked in HH:MM:SS format (24-hour style, no AM/PM).
+- Shfts can span across midnight.
+- program assumes all shifts occur within a 24-hour window (no multi-day shifts)
 
-** See PDF for sample input/output
+**See PDF for sample input/output**
 */
